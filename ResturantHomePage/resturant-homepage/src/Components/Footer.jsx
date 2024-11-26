994d33ea940d579ba5b5b7d6e104778682521cab
Brian <westbrayo@gmail.com>
2024-11-26 15:06:48 +0100

    footer styles

--- ResturantHomePage/resturant-homepage/src/Components/Footer.jsx
+++ ResturantHomePage/resturant-homepage/src/Components/Footer.jsx
@@ -4,6 +4,25 @@ const Footer = () => {
   return (
     <footer>
       <p>© 2024 Our Restaurant. All rights reserved.</p>
+      <div className="social-links">
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Facebook
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          Twitter
+        </a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram
+        </a>
+      </div>
     </footer>
   );
 };
